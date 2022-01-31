2c0ba710aab1faa37f20e2c4ba7b67afa92797b8
Asieertxo <asgal.gl@gmail.com>
2022-01-31 11:52:17 +0100

    Header de Pagina Home

--- src/components/Home/Header/Reservation.js
+++ src/components/Home/Header/Reservation.js
@@ -1,8 +1,17 @@
+import { useState } from "react"
+
 function Reservation() {
+  const [type, setType] = useState({
+    arrival: 'text', 
+    departure: 'text'
+
+  });
+
+
   return (
     <div className="mainheader-reservation">
-      <input placeholder="Entrada" type="text" onfocus="(this.type='date')" className="mainheader-reservation__date"></input>
-      <input placeholder="Salida" type="text" onfocus="(this.type='date')" className="mainheader-reservation__date"></input>
+      <input placeholder="Entrada" type={type.arrival} onFocus={() => setType({arrival:'date', departure:'text'})} onBlur={() => setType('text')} className="mainheader-reservation__date"></input>
+      <input placeholder="Salida" type={type.departure} onFocus={() => setType({arrival:'text', departure:'date'})} onBlur={() => setType('text')} className="mainheader-reservation__date"></input>
       <input type="button" className="boton" value="Reservar"></input>
     </div>
   );
