6902293457a335f1c6ffd736fa7399e9d957e103
Asieertxo <asgal.gl@gmail.com>
2022-02-23 21:12:14 +0100

    kk

--- src/components/Shop/Header/Popup/Search.js
+++ src/components/Shop/Header/Popup/Search.js
@@ -1,4 +1,15 @@
 import { debounce } from "./../../../../helpers/utils"
+export function debounce(f, timeout = 300){
+  let id = 0;
+  return function (...args){
+    if (id){
+      clearTimeout(id);
+    }
+    id = setTimeout(function(){
+      return f(...args);   
+    }, timeout)   
+  }
+}
 
 function Search(props) {
 
